b02757566cafb68b5855c6f15b5a822775674d2e
danhngan <danhnganc3@gmail.com>
2024-01-05 17:02:38 +0700

    edit single template

---
+++ web_data/wp_data/wp-content/plugins/test-query/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '08b563df257a7f95537c');
---
+++ web_data/wp_data/wp-content/plugins/test-query/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '74997b8989fb67764033');
